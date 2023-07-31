538b432ec3890283557da30c884320eab0d58090
DeeSpellman <thespellmanwitch@gmail.com>
2023-07-31 13:33:58 -0700

    Starting a practice project (Taco Cloud) to learn Spring with Spring In Action textbook.

---
+++ src/main/java/springinaction/tacocloudpractice/TacoCloudPracticeApplication.java
@@ -0,0 +1,13 @@
+package springinaction.tacocloudpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springinaction/tacocloudpractice/TacoCloudPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package springinaction.tacocloudpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
